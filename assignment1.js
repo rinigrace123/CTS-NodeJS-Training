f438e1bca9555390ee2e24759c5244ec3cc16909
Rini Grace Beera <81455859+rinigrace123@users.noreply.github.com>
2025-09-18 14:11:51 +0530

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,48 @@
+class Product {
+    #name;
+    constructor(name,price){
+        this.#name = name;
+        this.price= price;
+    }
+    
+    getName() {
+        return this.#name;
+    }
+}
+
+class ShoppingCart {
+    constructor(){
+        this.products = [];
+    }
+
+    addProduct(product){
+        this.products.push(product)
+    }
+
+    removeProduct(productName){
+        this.products = this.products.filter(product => product.getName() !== productName)
+    }
+
+    calculateTotal(){
+        return this.products.reduce((total,product)=> total+product.price,0)
+    }
+
+    displaySum(){
+        console.log(`Total Price : ${this.calculateTotal()}`)
+    }
+}
+
+//product istances
+const product1 = new Product('Spoon',20)
+const product2 = new Product('Plate',30)
+const product3 = new Product('Fork',10)
+
+const cart = new ShoppingCart();
+
+//adding products to cart
+cart.addProduct(product1);
+cart.addProduct(product2);
+cart.addProduct(product3);
+cart.displaySum()
+cart.removeProduct('Fork')
+cart.displaySum()
